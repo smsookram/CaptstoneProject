cc16a46fef3bb7c573aa4b83e8dfa6dce9422afd
smsookram <sm.sookram@gmail.com>
2025-06-26 19:26:18 -0400

    fixed API Key

--- config/weather_api.py
+++ config/weather_api.py
@@ -1,8 +1,15 @@
 import os
 from dotenv import load_dotenv
 
-# Load .env file
-load_dotenv()
+# Get the real path to .env inside weather_app/
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "weather_app"))
+env_path = os.path.join(project_root, ".env")
 
-# Access API key
+load_dotenv(dotenv_path=env_path)
+
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+# Temporary test
+print("API Key:", OPENWEATHER_API_KEY)
+
+
